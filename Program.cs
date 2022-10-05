f290460cf617ef95d899eb0c8db66756fe63b20c
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-05 15:10:12 -0300

    percorrendo a lista com for e foreach

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿int[] arrayInteiros = new int[4];
+﻿List <string> listaString = new List<string>();
 
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
-arrayInteiros[3] = 1;
+//Essa é a sintaxe de uma lista, não precisa determinar uma capacidade pra ela
 
-int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
-Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+listaString.Add("RJ");
+listaString.Add("ES");
+listaString.Add("MG");
 
-//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
-
-Console.WriteLine("Percorrendo o array com for");
-
-for(int contador = 0; contador < arrayInteiros.Length; contador++)
+Console.WriteLine("Percorrendo a lista com o for");
+for(int contador = 0; contador < listaString.Count; contador++)
 {
-    Console.WriteLine($"Posição N {contador} - {arrayInteiros[contador]}");
+    Console.WriteLine($"Posição N {contador} - {listaString[contador]}");
+}
+Console.WriteLine("Percorrendo a lista com o foreach");
+int contadorForeach = 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição N {contadorForeach} - {item}");
+    contadorForeach++;
 }
--- README.md
+++ README.md
@@ -68,5 +68,29 @@ for(int contador = 0; contador < arrayInteiros.Length; contador++)
     Console.WriteLine($"Posição N {contador} - {arrayInteiros[contador]}");
 }
 -----------------------------------------------------------------
+Trabalhando com listas com for e foreach:
+
+List <string> listaString = new List<string>();
+
+//Essa é a sintaxe de uma lista, não precisa determinar uma capacidade pra ela
+
+listaString.Add("RJ");
+listaString.Add("ES");
+listaString.Add("MG");
+
+Console.WriteLine("Percorrendo a lista com o for");
+for(int contador = 0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição N {contador} - {listaString[contador]}");
+}
+Console.WriteLine("Percorrendo a lista com o foreach");
+int contadorForeach = 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição N {contadorForeach} - {item}");
+    contadorForeach++;
+}
+-------------------------------------------------------------
+
 
 
